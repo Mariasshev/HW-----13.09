108eda220889a6daa78a7ccef67b683236f91455
Maria Shevchenko <sevcenkomaria72@gmail.com>
2023-09-17 15:39:48 +0300

    Добавьте файлы проекта.

---
+++ HW --- 13.09/DynamicArray.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include <iostream>
+using namespace std;
+
+class DynamicArray
+{
+	int* ptr; // pointer to dynamic array
+	int size;  //array size 
+public:
+	DynamicArray();
+	DynamicArray(int);
+	DynamicArray(const DynamicArray&); // copy constructor
+	~DynamicArray();
+	void Input();// rand
+	void Output()const;// console output
+	int* GetPointer()const;
+	int GetSize()const;
+
+	void Resize(int size);		//change size
+	void Sort();				//sort array
+	int Search(int a);			//find element in array
+	void Reverse();				//rewrite elements in reverse order
+
+	// реилизовать следующие методы:
+	//void ReSize(int size);// изменение размера массива
+	//void Sort();// сортировка(любой алгоритм сортировки)
+	//int Search(int a); // поиск элемента в массиве, возвращает индекс наиденного эл., либо -1.
+	//void Reverse(); // перезаписывает последовательность элементов в обратном порядке, напрмер 12345 -> 54321
+
+};
---
+++ HW --- 13.09/main.cpp
@@ -0,0 +1,53 @@
+﻿#pragma once
+#include "DynamicArray.h"
+using namespace std;
+
+DynamicArray GetObj() //возврат по значению
+{
+	DynamicArray obj(3);
+	obj.Input();
+
+	return obj;
+}
+void main()
+{
+	DynamicArray a(5); // constructor 1 param
+	a.Input();
+	a.Output();
+	DynamicArray b = a; // copy constructor
+	b.Output();
+
+	DynamicArray rezult = GetObj();
+	rezult.Output();
+	cout << endl << endl;
+
+
+	//resize
+	int userNum;
+	cout << "How many cells should we add?/ ";
+	cin >> userNum;
+	rezult.Resize(userNum);
+	cout << "Resize Result: " << endl;
+	rezult.Output();
+	cout << endl;
+
+	//sort
+	rezult.Sort();
+	cout << "Sort result: " << endl;
+	rezult.Output();
+	cout << endl;
+
+	//search
+	int userElement;
+	cout << "Enter number: ";
+	cin >> userElement;
+	cout << "Index: " << rezult.Search(userElement) << endl;
+	cout << endl;
+
+	//reverse
+	rezult.Reverse();
+	cout << "Reverse Result: " << endl;
+	rezult.Output();
+	cout << endl;
+
+}
